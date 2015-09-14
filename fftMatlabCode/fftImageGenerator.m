34b1a1d5a918fae8bca2202530a3d95290a7bb64
Bryce Summers <bryce@funtheemental.com>
2015-09-14 02:33:33 -0400

    Added fft code

---
+++ fftMatlabCode/fftImageGenerator.m
@@ -0,0 +1,92 @@
+close all; clear all;
+
+%2D FFT Demo
+
+%http://matlabgeeks.com/tips-tutorials/how-to-do-a-2-d-fourier-transform-in-matlab/
+
+% Import Images.
+imageA = imread('kayvonf3','jpg');
+%imageB = imread('MetroCenterEscalators','jpg');
+
+%Limits the channels so that the matrix is only 2D.
+imageA = imageA(:,:,1);
+%imageB = imageB(:,:,1);
+
+% Display images
+figure, imshow(imageA)
+title('Image A')
+%figure, imshow(imageB)
+%title('Image B')
+
+%Perform 2D FFTs
+fftA = fft2(double(imageA));
+
+% -- Band Limit the FFT.
+[rows, columns] = size(fftA);
+
+%for i=0:9
+
+fftB = fftshift(fftA);
+
+%lower = .1*i;
+%upper = lower + .1;
+
+upper = 1.0;
+
+for r=1:rows
+  for c=1:columns
+
+      dx = r - rows/2;
+      dy = c - columns/2;
+      
+      dist = sqrt(dx*dx + dy*dy);
+     
+      %if dist > rows/60 %lowpass
+      %if ~(rows/60 <= dist && dist < rows/20)
+      %if ~(rows/20 <= dist && dist < rows/10)%midpass
+      if (dist <= rows/10)%high pass
+          fftB(r,c) = 0;
+       
+      end
+      
+  end
+end
+
+
+%Display magnitude and phase of proccessed 2D FFT.
+figure, imshow(abs(fftB),[24 100000]), colormap gray
+set(gca,'position',[0 0 1 1],'units','normalized')
+%title('Image A FFT2 Magnitude')
+figure, imshow(angle(fftB),[-pi pi]), colormap gray
+%title('Image A FFT2 Phase')
+
+fftB = ifftshift(fftB);
+
+
+%Switch magnitude and phase of 2D FFTs.
+
+% Magnitude and phase based processing.
+%fftC = abs(fftA).*exp(i*angle(fftA));
+
+image_out = ifft2(fftB);
+
+%Calculate limits for plotting.
+cmin = min(min(abs(image_out)));
+cmax = max(max(abs(image_out)));
+
+%dmin = min(min(abs(imageD)));
+%dmax = max(max(abs(imageD)));
+
+%Display the proccessed image in an onscreen window.
+figure, imshow(abs(image_out), [cmin cmax]), colormap gray
+%title('Image Out')
+
+
+i = 0;
+%Save images.
+saveTightFigure(2 + 3*i,strcat('mag', int2str(i), int2str(i+1), '.png'))
+saveTightFigure(3 + 3*i,strcat('phase', int2str(i), int2str(i+1), '.png'))
+saveTightFigure(4 + 3*i,strcat('output', int2str(i), int2str(i+1), '.png'))
+
+%end
+
